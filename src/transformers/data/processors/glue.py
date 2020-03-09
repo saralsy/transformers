254e9c48f8837b21a4957d48616e336286f72e7f
saralsy <49797631+saralsy@users.noreply.github.com>
2020-03-09 00:13:26 -0400

    Update glue.py

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -514,7 +514,42 @@ class WnliProcessor(DataProcessor):
             label = line[-1]
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
+    
+class BoolqProcessor(DataProcessor):
+	"""Processor for the BoolQ data set (GLUE version)."""
+    def get_example_from_tensor_dict(self, tensor_dict):
+            """See base class."""
+            return InputExample(
+                tensor_dict["idx"].numpy(),
+                tensor_dict["question"].numpy().decode("utf-8"),
+                tensor_dict["passage"].numpy().decode("utf-8"),
+                str(tensor_dict["label"].numpy()),
+            )
 
+        def get_train_examples(self, data_dir):
+            """See base class."""
+            return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+        def get_dev_examples(self, data_dir):
+            """See base class."""
+            return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev_matched")
+
+        def get_labels(self):
+            """See base class."""
+            return ["true", "false"]
+
+        def _create_examples(self, lines, set_type):
+            """Creates examples for the training and dev sets."""
+            examples = []
+            for (i, line) in enumerate(lines):
+                if i == 0:
+                    continue
+                guid = "%s-%s" % (set_type, line[0])
+                question = line[1]
+                passage = line[2]
+                label = line[-1]
+                examples.append(InputExample(guid=guid, questiona=question, passage=passage, label=label))
+            return examples
 
 glue_tasks_num_labels = {
     "cola": 2,
@@ -526,6 +561,7 @@ glue_tasks_num_labels = {
     "qnli": 2,
     "rte": 2,
     "wnli": 2,
+    "boolq": 2
 }
 
 glue_processors = {
@@ -539,6 +575,7 @@ glue_processors = {
     "qnli": QnliProcessor,
     "rte": RteProcessor,
     "wnli": WnliProcessor,
+    "boolq": BoolQProcessor
 }
 
 glue_output_modes = {
@@ -552,4 +589,5 @@ glue_output_modes = {
     "qnli": "classification",
     "rte": "classification",
     "wnli": "classification",
+    "boolq": "classification"
 }
