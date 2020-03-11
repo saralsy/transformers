f2a2fa6a6ade9aeafa03c376eccd5e2420f21727
saralsy <49797631+saralsy@users.noreply.github.com>
2020-03-11 12:09:07 -0400

    Update glue.py

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -517,39 +517,43 @@ class WnliProcessor(DataProcessor):
     
 class BoolQProcessor(DataProcessor):
 	"""Processor for the BoolQ data set (GLUE version)."""
+	
     def get_example_from_tensor_dict(self, tensor_dict):
-            """See base class."""
-            return InputExample(
-                tensor_dict["idx"].numpy(),
-                tensor_dict["question"].numpy().decode("utf-8"),
-                tensor_dict["passage"].numpy().decode("utf-8"),
-                str(tensor_dict["label"].numpy()),
-            )
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["question"].numpy().decode("utf-8"),
+            tensor_dict["passage"].numpy().decode("utf-8"),
+            str(tensor_dict["label"].numpy()),
+        )
 
-        def get_train_examples(self, data_dir):
-            """See base class."""
-            return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
-        def get_dev_examples(self, data_dir):
-            """See base class."""
-            return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev_matched")
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
-        def get_labels(self):
-            """See base class."""
-            return ["true", "false"]
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
 
-        def _create_examples(self, lines, set_type):
-            """Creates examples for the training and dev sets."""
-            examples = []
-            for (i, line) in enumerate(lines):
-                if i == 0:
-                    continue
-                guid = "%s-%s" % (set_type, line[0])
-                question = line[1]
-                passage = line[2]
-                label = line[-1]
-                examples.append(InputExample(guid=guid, questiona=question, passage=passage, label=label))
-            return examples
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, line[0])
+            text_a = line[1]
+            text_b = line[2]
+            label = line[-1]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+    
+
+
 
 glue_tasks_num_labels = {
     "cola": 2,
